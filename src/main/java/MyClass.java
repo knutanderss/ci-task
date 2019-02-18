57b20f788c20154f2fdfa427aba16bbaed7ef75a
Knut Anders Stokke <knutandersstokke@gmail.com>
2019-02-18 12:14:15 +0100

    implement hello world

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by knutandersstokke on 18 18/02/2019.
+ */
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
